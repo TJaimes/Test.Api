748a5c9c4ce8e5771515aa406e92c11b7c187ac0
Tairi Jaimes <55193967+TJaimes@users.noreply.github.com>
2023-01-09 19:04:56 -0600

    Update ProductosController.cs

--- Api.Test/Controllers/ProductosController.cs
+++ Api.Test/Controllers/ProductosController.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 namespace Api.Test.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class ProductosController
     {
         [HttpGet ("GetProductos")]
