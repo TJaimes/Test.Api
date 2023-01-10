f7676a9ccb7e5e971a9cad1c5ea26d9a6ffb376a
Tairi Jaimes <tairijaimes12@gmail>
2023-01-09 18:31:51 -0600

    Agregar Proyecto

---
+++ Api.Test/Controllers/ProductosController.cs
@@ -0,0 +1,20 @@
+﻿using DA.Models;
+using DA.Providers;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Api.Test.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductosController
+    {
+        [HttpGet ("GetProductos")]
+        public async Task<string> GetProductos([FromHeader] string ProductName)
+        {
+            TestProvider productos = new TestProvider();
+            IEnumerable<Alphabetical_list_of_products> resultado = await productos.LeerProductos();
+            return JsonConvert.SerializeObject(resultado);
+        }
+    }
+}
---
+++ DA/Providers/TestProvider.cs
@@ -0,0 +1,28 @@
+﻿using DA.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DA.Providers
+{
+    public class TestProvider
+    {
+        public async Task<IEnumerable<Alphabetical_list_of_products>> LeerProductos()
+        {
+            try
+            {
+                using (NorthwindEntities context = new NorthwindEntities())
+                {
+                    return await context.Alphabetical_list_of_products.ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
